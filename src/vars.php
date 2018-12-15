69dc3e33eab354de7e974488f0c931ae313ccd46
Menno van Leeuwen <menno@vleeuwen.me>
2018-12-15 11:03:46 +0000

    Update vars.php

--- src/vars.php
+++ src/vars.php
@@ -9,10 +9,10 @@
     );
 
     $config = array(
-        'SQL_PASS' => "Not4You2",
+        'SQL_PASS' => "",
         'SQL_USER' => "root",
         'SQL_HOST' => "localhost",
-        'SQL_DB' => 'ct_kassa',
+        'SQL_DB' => 'cashier',
 		'timeout' => 480 // Session timeout
     );
 
